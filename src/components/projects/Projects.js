89455ba20db1516116d16a5e342e5a411ca62fed
Mandel <a.mandel06@gmail.com>
2021-03-27 06:55:20 +0100

    issue: hidden projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -4,28 +4,28 @@ import projectsList from './listeprojets';
 
 export default function Projects (){
 
-  const projectTab = projectsList.map(el =>
-    ( el.id%2 === 0 ?
-      (<div key={el.id} className="containerEachProject">
+  const projectTab = projectsList.map(skill =>
+    ( skill.id%2 === 0 ?
+      (<div key={skill.id} className="containerEachProject">
         <div className="eachProject__img">
-          <img src={require(`${el.img}`).default} alt={el.nom}/>
+          <img src={require(`${skill.img}`).default} alt={skill.nom}/>
         </div>
         <div className="eachProject__details">
-          <p><span>Nom : </span>{el.nom}</p>
-          <p><span>Description : </span> {el.description}</p>
-          <p><span>Technos : </span> {el.technologie}</p>
-          {el.visite ? <a href={el.lien } target="_blank" rel="noreferrer"><span>Lien : </span>CLIQUEZ ICI</a> : null}        </div>
+          <p><span>Nom : </span>{skill.nom}</p>
+          <p><span>Description : </span> {skill.description}</p>
+          <p><span>Technos : </span> {skill.technologie}</p>
+          {skill.visite ? <a href={skill.lien } target="_blank" rel="noreferrer"><span>Lien : </span>CLIQUEZ ICI</a> : null}        </div>
       </div>)
       :
-      (<div key={el.id} className="containerEachProject__reverse">
+      (<div key={skill.id} className="containerEachProject__reverse">
         <div className="eachProject__details__reverse">
-        <p><span>Nom : </span>{el.nom}</p>
-          <p><span>Description : </span> {el.description}</p>
-          <p><span>Technos : </span> {el.technologie}</p>
-          {el.visite ? <a href={el.lien } target="_blank" rel="noreferrer"><span>Lien : </span>CLIQUEZ ICI</a> : null}
+        <p><span>Nom : </span>{skill.nom}</p>
+          <p><span>Description : </span> {skill.description}</p>
+          <p><span>Technos : </span> {skill.technologie}</p>
+          {skill.visite ? <a href={skill.lien } target="_blank" rel="noreferrer"><span>Lien : </span>CLIQUEZ ICI</a> : null}
         </div>
         <div className="eachProject__img__reverse">
-          <img src={require(`${el.img}`).default} alt={el.nom}/>
+          <img src={require(`${skill.img}`).default} alt={skill.nom}/>
         </div>
       </div>)
     )
