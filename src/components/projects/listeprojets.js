f4bb70a5f08d35d3ae0ecf582ea3816dcf25c32a
Mandel <a.mandel06@gmail.com>
2021-03-31 02:50:27 +0200

    description carnet adresse

--- src/components/projects/listeprojets.js
+++ src/components/projects/listeprojets.js
@@ -32,7 +32,7 @@ const projectsList = [
         id:3,
         nom:"Carnet d'adresses",
         img:'./imgprojets/appartoo.png',
-        description:"Projet Carnet d'adresse. Fonctionnalité et design spécifiés par l'entreprise.",
+        description:"Projet Carnet d'adresse réalisé en 3 jours. Fonctionnalité et design spécifiés par l'entreprise.",
         technologie : 'Angular 9 et Node js', 
         visite : false
     },
